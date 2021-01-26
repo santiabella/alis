2395c084dffec3fd9398dae75868255c6fab1566
santiabella <64903155+santiabella@users.noreply.github.com>
2021-01-26 08:46:00 +0100

    Update alis.sh

--- alis.sh
+++ alis.sh
@@ -582,14 +582,15 @@ function partition() {
         mount -o "$PARTITION_OPTIONS" "$DEVICE_ROOT" /mnt
         btrfs subvolume create /mnt/@
         btrfs subvolume create /mnt/@home
+	btrfs subvolume create /mnt/@var
         umount /mnt
 
         mount -o "subvol=@,$PARTITION_OPTIONS,compress=zstd" "$DEVICE_ROOT" /mnt
 
-        mkdir /mnt/{boot,home}
+        mkdir /mnt/{boot,home,var}
         mount -o "$PARTITION_OPTIONS_BOOT" "$PARTITION_BOOT" /mnt/boot
         mount -o "subvol=@home,$PARTITION_OPTIONS_ROOT,compress=zstd" "$DEVICE_ROOT" /mnt/home
-        
+        mount -o "subvol=@var,$PARTITION_OPTIONS_ROOT,compress=zstd" "$DEVICE_ROOT" /mnt/var
     else
         mount -o "$PARTITION_OPTIONS_ROOT" "$DEVICE_ROOT" /mnt
 
